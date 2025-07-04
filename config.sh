433fe1276a42ba9ea797d79eabc599c77922de96
abdulqadir <66162097+alone412@users.noreply.github.com>
2025-07-04 10:24:48 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,46 +3,41 @@
 # Kernel name
 KERNEL_NAME="QuartiX-v2"
 
-# GKI Version
-GKI_VERSION="android12-5.10"
+# GKI Version (use correct GKI branch for OnePlus 12R)
+GKI_VERSION="android13-5.15"
 
-# Build variables
-export TZ="Asia/Makassar"
-export KBUILD_BUILD_USER="eraselk"
+# Build identity
+export TZ="Asia/Kolkata"
+export KBUILD_BUILD_USER="abdul"
 export KBUILD_BUILD_HOST="$KERNEL_NAME"
 export KBUILD_BUILD_TIMESTAMP=$(date)
 
-# AnyKernel variables
-ANYKERNEL_REPO="https://github.com/hazepynut/anykernel"
+# AnyKernel3
+ANYKERNEL_REPO="https://github.com/TanakaLun/AnyKernel3"
 ANYKERNEL_BRANCH="gki"
 
-# Kernel
-KERNEL_REPO="https://github.com/hazepynut/kernel_new"
-KERNEL_BRANCH="android12-5.10"
+# Kernel source
+KERNEL_REPO="https://github.com/OnePlusOSS/android_kernel_oneplus_sm8550"
+KERNEL_BRANCH="oneplus/SM8550_A13"
+
+# Defconfig
 KERNEL_DEFCONFIG="gki_defconfig"
 DEFCONFIG_FILE="$workdir/common/arch/arm64/configs/$KERNEL_DEFCONFIG"
 
-# Defconfigs would be merged in the compiling processes
-DEFCONFIGS_EXAMPLE="
-vendor/xiaomi.config
-vendor/gold.config
-"
-DEFCONFIGS="
-" # Leave this empty if you don't need to merge any configs
+# Optional merge defconfigs
+DEFCONFIGS=""
 
-# Releases repository
-GKI_RELEASES_REPO="https://github.com/hazepynut/quartix-releases"
+# Release target repo (used in artifacts/info.txt)
+GKI_RELEASES_REPO="https://github.com/alone412/12r-kernel-releases"
 
-# AOSP Clang
+# Toolchain: Use custom Clang from RvClang
 USE_AOSP_CLANG="false"
-AOSP_CLANG_SOURCE="r547379" # Should be version number or direct link to clang tarball
+AOSP_CLANG_SOURCE=""
 
-# Custom clang
 USE_CUSTOM_CLANG="true"
 CUSTOM_CLANG_SOURCE="https://github.com/Rv-Project/RvClang/releases/download/20.1.0/RvClang-20.1.0-bolt-pgo-full_lto.tar.gz"
 CUSTOM_CLANG_BRANCH=""
 
-# Zip name
+# Build zip name
 BUILD_DATE=$(date -d "$KBUILD_BUILD_TIMESTAMP" +"%Y-%m-%d-%H%M")
-ZIP_NAME="$KERNEL_NAME-KVER-VARIANT.zip"
-# Note: KVER and VARIANT are placeholder and they will be changed in the build.sh script.
+ZIP_NAME="$KERNEL_NAME-KVER-VARIANT.zip"  # Will be auto-filled later
